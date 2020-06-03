827a3750ba42414b788f1b3791406fef3fd86be7
pabloandreychacon <pabloandreychacon@hotmail.com>
2020-06-02 22:18:26 -0600

    Updates

---
+++ precache-manifest.a630b19171fa37547adf7ca3c2dbab9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5299c09e389b5461fde30d171465af9d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2cd481bf3c36893e4b56",
+    "url": "/my-app/static/css/2.b7e16ca0.chunk.css"
+  },
+  {
+    "revision": "9a619fd052cfa63a97ef",
+    "url": "/my-app/static/css/main.4a1fabd3.chunk.css"
+  },
+  {
+    "revision": "2cd481bf3c36893e4b56",
+    "url": "/my-app/static/js/2.4192a6bd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-app/static/js/2.4192a6bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a619fd052cfa63a97ef",
+    "url": "/my-app/static/js/main.ecc89497.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "3972b31188a893b12efd38bf2cf73403",
+    "url": "/my-app/static/media/hero-bg.3972b311.jpg"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/my-app/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/my-app/static/media/icofont.f6ab04ae.woff"
+  }
+]);
